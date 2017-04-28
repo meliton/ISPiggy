af18754a0e404b77583b0866c52f0430da294a75
Mel <m_hinojosa@rocketmail.com>
2017-04-28 00:13:14 -0500

    Update Form1.Designer.vb

--- ISPiggyVB17/Form1.Designer.vb
+++ ISPiggyVB17/Form1.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -34,16 +34,18 @@ Partial Class Form1
         Me.lblName = New System.Windows.Forms.Label()
         Me.txtDomain = New System.Windows.Forms.TextBox()
         Me.btnIndicator = New System.Windows.Forms.Button()
-        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
-        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
         Me.chkDebug = New System.Windows.Forms.CheckBox()
         Me.ni = New System.Windows.Forms.NotifyIcon(Me.components)
-        Me.StatusStrip1.SuspendLayout()
+        Me.Rnd01 = New System.Windows.Forms.Button()
+        Me.stat1 = New System.Windows.Forms.StatusStrip()
+        Me.stat01 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblLoopCheck = New System.Windows.Forms.Label()
+        Me.stat1.SuspendLayout()
         Me.SuspendLayout()
         '
         'RandomName
         '
-        Me.RandomName.Location = New System.Drawing.Point(330, 29)
+        Me.RandomName.Location = New System.Drawing.Point(341, 29)
         Me.RandomName.Name = "RandomName"
         Me.RandomName.Size = New System.Drawing.Size(70, 23)
         Me.RandomName.TabIndex = 0
@@ -52,7 +54,7 @@ Partial Class Form1
         '
         'GetIP
         '
-        Me.GetIP.Location = New System.Drawing.Point(417, 31)
+        Me.GetIP.Location = New System.Drawing.Point(417, 59)
         Me.GetIP.Name = "GetIP"
         Me.GetIP.Size = New System.Drawing.Size(70, 23)
         Me.GetIP.TabIndex = 1
@@ -61,7 +63,7 @@ Partial Class Form1
         '
         'ToCom
         '
-        Me.ToCom.Location = New System.Drawing.Point(417, 5)
+        Me.ToCom.Location = New System.Drawing.Point(417, 29)
         Me.ToCom.Name = "ToCom"
         Me.ToCom.Size = New System.Drawing.Size(70, 23)
         Me.ToCom.TabIndex = 2
@@ -88,18 +90,20 @@ Partial Class Form1
         '
         'btnStart
         '
+        Me.btnStart.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnStart.Location = New System.Drawing.Point(12, 29)
         Me.btnStart.Name = "btnStart"
-        Me.btnStart.Size = New System.Drawing.Size(75, 35)
+        Me.btnStart.Size = New System.Drawing.Size(75, 53)
         Me.btnStart.TabIndex = 5
         Me.btnStart.Text = "START"
         Me.btnStart.UseVisualStyleBackColor = True
         '
         'btnStop
         '
+        Me.btnStop.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnStop.Location = New System.Drawing.Point(107, 29)
         Me.btnStop.Name = "btnStop"
-        Me.btnStop.Size = New System.Drawing.Size(75, 35)
+        Me.btnStop.Size = New System.Drawing.Size(75, 53)
         Me.btnStop.TabIndex = 6
         Me.btnStop.Text = "STOP"
         Me.btnStop.UseVisualStyleBackColor = True
@@ -107,48 +111,36 @@ Partial Class Form1
         'lblName
         '
         Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblName.Location = New System.Drawing.Point(9, 8)
         Me.lblName.Name = "lblName"
-        Me.lblName.Size = New System.Drawing.Size(45, 13)
+        Me.lblName.Size = New System.Drawing.Size(59, 16)
         Me.lblName.TabIndex = 7
         Me.lblName.Text = "lblName"
         '
         'txtDomain
         '
-        Me.txtDomain.Location = New System.Drawing.Point(315, 5)
+        Me.txtDomain.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDomain.Location = New System.Drawing.Point(330, 5)
         Me.txtDomain.Name = "txtDomain"
-        Me.txtDomain.Size = New System.Drawing.Size(96, 20)
+        Me.txtDomain.Size = New System.Drawing.Size(151, 21)
         Me.txtDomain.TabIndex = 9
         '
         'btnIndicator
         '
-        Me.btnIndicator.Location = New System.Drawing.Point(196, 3)
+        Me.btnIndicator.Location = New System.Drawing.Point(211, 5)
         Me.btnIndicator.Name = "btnIndicator"
-        Me.btnIndicator.Size = New System.Drawing.Size(42, 23)
+        Me.btnIndicator.Size = New System.Drawing.Size(88, 28)
         Me.btnIndicator.TabIndex = 10
         Me.btnIndicator.UseVisualStyleBackColor = True
-        '
-        'StatusStrip1
-        '
-        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
-        Me.StatusStrip1.Location = New System.Drawing.Point(0, 72)
-        Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(571, 22)
-        Me.StatusStrip1.TabIndex = 11
-        Me.StatusStrip1.Text = "Status"
-        '
-        'ToolStripStatusLabel1
-        '
-        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
-        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(121, 17)
-        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
         '
         'chkDebug
         '
         Me.chkDebug.AutoSize = True
-        Me.chkDebug.Location = New System.Drawing.Point(196, 39)
+        Me.chkDebug.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkDebug.Location = New System.Drawing.Point(236, 39)
         Me.chkDebug.Name = "chkDebug"
-        Me.chkDebug.Size = New System.Drawing.Size(58, 17)
+        Me.chkDebug.Size = New System.Drawing.Size(63, 19)
         Me.chkDebug.TabIndex = 12
         Me.chkDebug.Text = "Debug"
         Me.chkDebug.UseVisualStyleBackColor = True
@@ -159,13 +151,50 @@ Partial Class Form1
         Me.ni.Text = "ISPiggy"
         Me.ni.Visible = True
         '
+        'Rnd01
+        '
+        Me.Rnd01.Location = New System.Drawing.Point(487, 59)
+        Me.Rnd01.Name = "Rnd01"
+        Me.Rnd01.Size = New System.Drawing.Size(70, 23)
+        Me.Rnd01.TabIndex = 13
+        Me.Rnd01.Text = "Rnd 0 or 1"
+        Me.Rnd01.UseVisualStyleBackColor = True
+        '
+        'stat1
+        '
+        Me.stat1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.stat01})
+        Me.stat1.Location = New System.Drawing.Point(0, 90)
+        Me.stat1.Name = "stat1"
+        Me.stat1.Size = New System.Drawing.Size(564, 22)
+        Me.stat1.TabIndex = 15
+        Me.stat1.Text = "Stat1"
+        '
+        'stat01
+        '
+        Me.stat01.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.stat01.Name = "stat01"
+        Me.stat01.Size = New System.Drawing.Size(43, 17)
+        Me.stat01.Text = "stat01"
+        '
+        'lblLoopCheck
+        '
+        Me.lblLoopCheck.AutoSize = True
+        Me.lblLoopCheck.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLoopCheck.Location = New System.Drawing.Point(193, 69)
+        Me.lblLoopCheck.Name = "lblLoopCheck"
+        Me.lblLoopCheck.Size = New System.Drawing.Size(91, 16)
+        Me.lblLoopCheck.TabIndex = 16
+        Me.lblLoopCheck.Text = "lblLoopCheck"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(571, 94)
+        Me.ClientSize = New System.Drawing.Size(564, 112)
+        Me.Controls.Add(Me.lblLoopCheck)
+        Me.Controls.Add(Me.stat1)
+        Me.Controls.Add(Me.Rnd01)
         Me.Controls.Add(Me.chkDebug)
-        Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.btnIndicator)
         Me.Controls.Add(Me.txtDomain)
         Me.Controls.Add(Me.lblName)
@@ -179,8 +208,8 @@ Partial Class Form1
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        Me.StatusStrip1.ResumeLayout(False)
-        Me.StatusStrip1.PerformLayout()
+        Me.stat1.ResumeLayout(False)
+        Me.stat1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -196,8 +225,10 @@ Partial Class Form1
     Friend WithEvents lblName As Label
     Friend WithEvents txtDomain As TextBox
     Friend WithEvents btnIndicator As Button
-    Friend WithEvents StatusStrip1 As StatusStrip
-    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
     Friend WithEvents chkDebug As CheckBox
     Friend WithEvents ni As NotifyIcon
+    Friend WithEvents Rnd01 As Button
+    Friend WithEvents stat1 As StatusStrip
+    Friend WithEvents stat01 As ToolStripStatusLabel
+    Friend WithEvents lblLoopCheck As Label
 End Class
